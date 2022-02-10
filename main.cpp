087fc2b497c86a3f0881819d98439283af7b383d
unknown <kjaroszewski21@gmail.com>
2022-02-10 23:35:53 +0100

    funkcja calkujaca

--- main.cpp
+++ main.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
-
 #include "vector.h"
 #include "Cialo.h"
 #include "output.h"
+
+
+void funkcja(std::vector<Cialo_Niebieskie> &cialo,int krok)
+{
+	const double G =6.67259 * pow(10,-11);
+	for( int i =0 ; i < cialo.size() ; i++)
+		cialo[i].acc = {0,0,0};
+	for( int i =0 ; i < cialo.size() ; i++)
+		for ( int j = i+1 ; j < cialo.size() ; j++)
+		{
+			double sila= G * ( (cialo[i].masa *cialo[j].masa)/ mag_squared(cialo[i].pos-cialo[j].pos));
+			cialo[i].acc += normalized(cialo[i].pos-cialo[j].pos) *(sila / cialo[i].masa) ;
+			cialo[j].acc += -normalized(cialo[i].pos-cialo[j].pos) *(sila / cialo[j].masa) ;
+		}
+	for( auto & cialo1:cialo)
+	{
+		cialo1.vel += cialo1.acc*krok;
+		cialo1.pos += cialo1.vel*krok;
+	}
+}
+
 
 int main()
 {
@@ -15,14 +35,6 @@ int main()
 	std::ofstream plik_wyjsciowy = zainicjalizuj_plik_wejsciowy(ciala, krok_czasowy, "wyjscie.txt");
 	wypisz_krok_do_pliku(ciala, plik_wyjsciowy);
 
-	for(int i = 0; i < 100; i++)
-	{
-		ciala[0].pos *= 1024;
-		ciala[0].pos /= 1023;
-		ciala[1].pos *= 1024;
-		ciala[1].pos /= 1023;
-		wypisz_krok_do_pliku(ciala, plik_wyjsciowy);
-	}
 	return 0;
 
 }
